6ab17b769f244d3aa242096cc82c52feaa557d3f
jirayut <>
2025-07-04 08:04:48 +0700

    เปลี่นนสีtext ปุ่ม

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -189,7 +189,7 @@ const handleCheckboxChange = () => {
         <button
           :disabled="checkboxRef === false || !firstname || !lastname"
           @click="onSubmit"
-          class="px-4 py-2 w-100 bg-yellow-500 text-white rounded-md hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-400"
+          class="px-4 py-2 w-100 bg-yellow-500 text-black rounded-md hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-400"
         >
           {{ $t("action_button") }}
         </button>
